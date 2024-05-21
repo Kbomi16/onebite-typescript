4f628fd046b7aef36665e39120ff337b6b4fde6a
김보미 <kimbomi2172@naver.com>
2024-05-21 17:34:07 +0900

    S2: 배열과 튜플

--- S2/dist/chap1.js
+++ S2/dist/chap1.js
@@ -1,3 +1,10 @@
-// number
 let num1 = 123;
+let str1 = '아';
+let str2 = `아 + ${num1}`;
+let bool1 = true;
+let null1 = null;
+let undefined1 = undefined;
+// 리터럴 타입: 한 번 지정하면 수정할 수 없음
+let numA = 10;
 export {};
+// error: numA = 12
